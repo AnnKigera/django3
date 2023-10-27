7cf169ff0a30ddabd36e40b4700e215584844b46
AnnKigera <wairimukigera001@gmail.com>
2023-10-28 02:12:20 +0300

    CRUD

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
 
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
 #homepage
 def home(request):
 
@@ -22,3 +25,27 @@ def register(request):
     context = {'form': form}
 
     return render(request, 'webapp/register.html', context=context)
+
+# login a user
+def my_login(request):
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+
+                #return redirect('')
+            
+
+    context = {'form':form}
+
+    return render(request, 'webapp/my-login.html', context=context)
+
