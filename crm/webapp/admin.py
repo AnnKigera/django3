a044e8e13e7c11a969ce1cc454c9e7587aa9f8b2
AnnKigera <wairimukigera001@gmail.com>
2023-11-04 02:58:05 +0300

    progress

--- crm/webapp/admin.py
+++ crm/webapp/admin.py
@@ -1,7 +1,14 @@
+# admin.py
+
 from django.contrib import admin
+from .models import UserProfile, Patient, Doctor, MedicalRecord, DiabetesPrediction, Comment, Record
 
-# Register your models here.
+# Register your models here
 
-from . models import Record
-
+admin.site.register(UserProfile)
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(MedicalRecord)
+admin.site.register(DiabetesPrediction)
+admin.site.register(Comment)
 admin.site.register(Record)
--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -1,4 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        full_name = models.CharField(max_length=255,primary_key=True)
+        email = models.CharField(max_length=255)
+        age = models.IntegerField()
+        gender = models.CharField(max_length=10)
+
+class Patient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=15)
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=15)
+
+
+class MedicalRecord(models.Model):
+        user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+        timestamp = models.DateTimeField(auto_now_add=True)
+        bmi = models.FloatField()
+        glucose_level = models.FloatField()
+        insulin_level = models.FloatField()
+
+
+class DiabetesPrediction(models.Model):
+        user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+        timestamp = models.DateTimeField(auto_now_add=True)
+        predicted_diabetes = models.BooleanField()
+        prediction_accuracy = models.FloatField()
+
+
+class Comment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    text = models.TextField()
+
 
 class Record(models.Model):
 
@@ -22,4 +67,4 @@ class Record(models.Model):
 
 
     def __str__(self):
-        return self.first_name + "   " + self.last_name
+        return f"{self.user.user.username} - {self.timestamp}"
